b324a3d67d0f5a1324a846b0a95ceefe7de515d2
Your friendly neighborhood <120403809+LeweProjects@users.noreply.github.com>
2025-03-24 17:48:40 +0800

    add restricted message

--- src/app/route.js
+++ src/app/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json({ message: process.env.DB_USER });
+  return NextResponse.json({ message: "You are not allowed here!" });
 }
